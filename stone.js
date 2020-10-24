2620f6c379caee1788ebb8779ba658fd5dffe01f
shubh-darwade <70751205+shubh-darwade@users.noreply.github.com>
2020-10-24 09:23:25 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -2,9 +2,8 @@ class stone
 {
     constructor(x,y,radius)
     {
-        this.body=Bodies.circle(x,y,radius/2,{isStatic:false,restitution:0,friction:0,});
-        this.width=radius;
-        this.height=radius;
+        this.r=radius;
+        this.body=Bodies.circle(x,y,this.r,{isStatic:false,restitution:0,friction:0,});
         this.image=loadImage("images/stone.png")
         World.add(world,this.body);
     }
@@ -12,7 +11,7 @@ class stone
       {
           var spos=this.body.position;
          imageMode(CENTER);
-         image(this.image,spos.x,spos.y,this.width,this.height);
+         image(this.image,spos.x,spos.y,this.r*2,this.r*2);
 
       }
 }
